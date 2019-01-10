9829b8898ce6cb213faefef3e74278674a915904
speciallan <350394776@qq.com>
2019-01-10 20:02:36 +0800

    init

---
+++ leetcode/problem1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> m;
+            vector<int> res;
+        }
+
+
+
+};
---
+++ leetcode/problem1_2.cpp
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 1;
+    int b = 2;
+    cout << a+b << endl;
+
+    return 0;
+}
+
+
+
