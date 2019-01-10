deaf27fa781a56c64576087866ef205b36edb27b
speciallan <350394776@qq.com>
2019-01-10 20:15:36 +0800

    test

--- leetcode/problem1.cpp
+++ leetcode/problem1.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
+// #include <vector>
+// #include <unordered_map>
 
 using namespace std;
 
-class Solution {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            unordered_map<int, int> m;
-            vector<int> res;
-        }
+// class Solution {
+//     public:
+//         vector<int> twoSum(vector<int>& nums, int target) {
+//             // unordered_map<int, int> m;
+//             // vector<int> res;
+//         }
+// };
 
+int main(int argc, char const *argv[])
+{
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
 
+    cout << target << endl;
 
-};
+    return 0;
+}
