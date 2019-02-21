41d7e97d2dfb8f3637115528b32761893c225fbc
speciallan <350394776@qq.com>
2019-02-21 13:40:43 +0800

    123

--- leetcode/problem123/problem123.py
+++ leetcode/problem123/problem123.py
@@ -1,13 +1,30 @@
 class Solution:
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
 
-        profit = 0
+        n = len(prices)
+
+        if n < 2:
+            return 0
+
+        dp1 = [0 for _ in range(n)]
+        dp2 = [0 for _ in range(n)]
+
+        minval = prices[0]
+        maxval = prices[-1]
+
+        #前向   
+        for i in range(1,n):
+            dp1[i] = max(dp1[i-1], prices[i] - minval)
+            minval = min(minval, prices[i])
+
+        #后向    
+        for i in range(n-2,-1,-1):
+            dp2[i] = max(dp2[i+1], maxval - prices[i])
+            maxval = max(maxval, prices[i])
         
-        return profit
+        dp = [dp1[i] + dp2[i] for i in range(n)]
+
+        return max(dp)
 
 if __name__ == "__main__":
 
